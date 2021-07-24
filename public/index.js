d695be7059589176a672e351a60a7f670e17066e
Jeremy Brown <jeremy@geistinteractive.com>
2021-07-23 21:09:49 -0400

    changes made with stephan casas

--- public/index.js
+++ public/index.js
@@ -10,6 +10,30 @@ const data = [
 ];
 const value = 1;
 
+data.forEach((item) => {
+  const template = `<div
+            class="
+              sample
+              h-16
+              flex
+              item-center
+              bg-blue-300
+              text-white
+              font-semibold
+            "
+          >
+          <div class="flex items-center space-x-2 p-2">
+          <input type="checkbox" data--itemId="${item.id}" />
+            <div class="mx-">${item.label}</div>
+            <div>
+          </div>`;
+  const el = document.createElement("div");
+  el.innerHTML = template;
+  const d = el.firstChild;
+  document.querySelector("#canvas").appendChild(d);
+});
+
+/*
 data.forEach((i) => {
   const cb = document.createElement("input");
   const d = document.createElement("div");
@@ -27,11 +51,18 @@ data.forEach((i) => {
   d.appendChild(labelForBox);
   form.appendChild(d);
 });
+*/
 
-const onSubmit = () => {
-  const values = form.getElementsByTagName("input");
-  console.log(values[0]);
-};
+document.querySelector("button").addEventListener("click", (e) => {
+  e.preventDefault();
+  const values = [...document.querySelectorAll('input[type="checkbox"]')].map(
+    (input) => ({
+      value: input.checked,
+      id: input.getAttribute("data--itemId"),
+    })
+  );
+  console.log(values);
+});
 // document.getElementById("dataList").addEventListener("onsubmit", () => {
 //   const values = form.getElementsByTagName("input");
 //   console.log(values);
